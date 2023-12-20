775e5fff3cbf0009e5e2a175d592117662af0ae0
Stefan <smig88@gmail.com>
2023-12-20 16:46:03 -0500

    fix(build): docker auth and push

--- build.prod.sh
+++ build.prod.sh
@@ -8,3 +8,5 @@ docker build . -t 142412772381.dkr.ecr.eu-west-1.amazonaws.com/prodigy-quesmed-c
   --build-arg="POSTGRES_USER=$POSTGRES_USER" \
   --build-arg="POSTGRES_PASSWORD=$POSTGRES_PASSWORD" \
   --build-arg="POSTGRES_DB=$POSTGRES_DB"
+aws ecr get-login-password | docker login --username AWS --password-stdin 142412772381.dkr.ecr.eu-west-1.amazonaws.com
+docker push 142412772381.dkr.ecr.eu-west-1.amazonaws.com/prodigy-quesmed-comments
